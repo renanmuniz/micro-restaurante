8bcf2900e30e15d02b91b56eda6c07a50e6110ab
Renan Muniz Merlo <renan.mmerlo@gmail.com>
2021-05-27 22:57:49 -0300

    Criacao do projeto micro-restaurante-cliente

---
+++ micro-restaurante-cliente/src/main/java/com/cliente/microrestaurante/MicroRestauranteClienteApplication.java
@@ -0,0 +1,13 @@
+package com.cliente.microrestaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroRestauranteClienteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroRestauranteClienteApplication.class, args);
+    }
+
+}
---
+++ micro-restaurante-cliente/src/test/java/com/cliente/microrestaurante/MicroRestauranteClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cliente.microrestaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroRestauranteClienteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
