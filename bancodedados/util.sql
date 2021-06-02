659dc51d01a15f35be5d1ee9b0dca97f8d3624e4
Renan Muniz Merlo <renan.mmerlo@gmail.com>
2021-06-01 23:11:42 -0300

    1)Alterado a tabela de produto e preco. 2)Criado endpoint de produto

--- bancodedados/util.sql
+++
@@ -1,9 +0,0 @@
-Select que traz o ultimo preco de um produto:
-
-SELECT produto.id, produto.nome, produto.descricao, preco.id, preco.preco
-FROM produto 
-JOIN preco_produto on (produto.id=preco_produto.id_produto)
-JOIN preco on (preco.id=preco_produto.id_preco)
-where produto.id=2
-order by preco.dthrcriacao DESC
-limit 1;
