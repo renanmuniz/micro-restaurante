cd0bd3bee0ba62c091044c41e23781832264a320
renanmuniz <renan.mmerlo@gmail.com>
2021-06-10 18:01:31 -0300

    Criado projeto micro-creditcard

---
+++ micro-creditcard/src/main/java/com/microcreditcard/MicroCreditcardApplication.java
@@ -0,0 +1,17 @@
+package com.microcreditcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSpringDataWebSupport
+@EnableSwagger2
+public class MicroCreditcardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroCreditcardApplication.class, args);
+    }
+
+}
---
+++ micro-creditcard/src/test/java/com/microcreditcard/MicroCreditcardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microcreditcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroCreditcardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
